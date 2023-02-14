c5766877d4d74cbbde8aedc9c23838094ec33266
Douglas Andre da Silva Teodoro <douglasandre.s.t@gamil.com>
2023-02-14 18:56:21 -0300

    Adicionar arquivos de projeto.

---
+++ PequenaAngel.Api/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PequenaAngel.Api
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
